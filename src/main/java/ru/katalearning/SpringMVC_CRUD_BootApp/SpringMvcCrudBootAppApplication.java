1d5963df234068efc014a08acbb96fb7309e12b0
Andrey Belov <belovandr96@gmail.com>
2023-05-29 00:05:25 +0500

    Initial commit

---
+++ src/main/java/ru/katalearning/SpringMVC_CRUD_BootApp/SpringMvcCrudBootAppApplication.java
@@ -0,0 +1,13 @@
+package ru.katalearning.SpringMVC_CRUD_BootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCrudBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcCrudBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/katalearning/SpringMVC_CRUD_BootApp/SpringMvcCrudBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.katalearning.SpringMVC_CRUD_BootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcCrudBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
